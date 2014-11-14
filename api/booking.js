5caac79b70597f117eca1d2807a24a71299e245b
Alexey Zhelonkin <Alexey.Zhelonkin@paxport.se>
2014-11-14 14:02:38 +0100

    fix booking.js

--- api/booking.js
+++ api/booking.js
@@ -1,6 +1,7 @@
 var Q = require('q');
+var wait = require('wait.for');
 
-module.exports = function(params) {
+module.exports = function (params) {
   var module = {};
 
   module.verbose = params.verbose;
@@ -11,12 +12,27 @@ module.exports = function(params) {
 
   module.baseUrl = params.baseUrl;
 
+
+  module.client.qGet = function (url, args, stdCallback) {
+    this.get(url, args, function (data, response) {
+      return stdCallback(null, {data: data, response: response});
+    });
+  };
+
+  module.client.qPost = function (url, args, stdCallback) {
+    this.post(url, args, function (data, response) {
+      return stdCallback(null, {data: data, response: response});
+    });
+  };
+
+
+
   /*
-  GET /bookings/{bookingId}/detail
-  todo: DK
+   GET /bookings/{bookingId}/detail
+   todo: DK
    */
-  module.get = function(bid) {
-    return Q.Promise(function(resolve, reject, notify) {
+  module.get = function (bid) {
+    return Q.Promise(function (resolve, reject, notify) {
 
       var args = {
         headers: { "Content-Type": "application/xml", "Authorization": 'Basic ' + module.apikey }
@@ -25,7 +41,7 @@ module.exports = function(params) {
       if (module.verbose) {
         console.log('GET ' + url + ' ...');
       }
-      module.client.get(url, args, function(data, response) {
+      module.client.get(url, args, function (data, response) {
 
         if (response.statusCode === 401) {
           reject("401 - API key required");
@@ -41,12 +57,49 @@ module.exports = function(params) {
     });
   };
 
+  module.getSync = function (bid) {
+    var args = {
+      headers: { "Content-Type": "application/xml", "Authorization": 'Basic ' + module.apikey }
+    };
+    var url = module.baseUrl + '/bookings/' + bid + '/detail';
+
+    var result = wait.forMethod(module.client, "qGet", url, args);
+
+    if (result.response.statusCode === 401) {
+      throw new Error("401 - API key required");
+    } else if (result.response.statusCode !== 200) {
+      throw new Error(result.response.statusCode + " - Something went wrong... :-(");
+    }
+    return result.data;
+  };
+
+
+  module.postSync = function (bxml) {
+    var args = {
+      data: bxml,
+      headers: { "Content-Type": "application/xml", "Authorization": 'Basic ' + module.apikey }
+    };
+    var url = module.baseUrl + '/bookings';
+    if (module.verbose) {
+      console.log('POST ' + url + ' with XML ...');
+    }
+
+    var result =  wait.forMethod(module.client, "qPost", url, args);
+    if (result.response.statusCode === 401) {
+      throw new Error("401 - API key required");
+    } else if (result.response.statusCode !== 201) {
+      throw new Error(result.response.statusCode + " - Something went wrong... :-(");
+    }
+    var boid = result.response.headers.location.substring(result.response.headers.location.lastIndexOf('/') + 1);
+    boid = boid.replace('#', '')
+    return boid;
+  };
   /*
-  POST /bookings/
-  todo DK
+   POST /bookings/
+   todo DK
    */
-  module.post = function(bxml) {
-    return Q.Promise(function(resolve, reject, notify) {
+  module.post = function (bxml) {
+    return Q.Promise(function (resolve, reject, notify) {
 
       var args = {
         data: bxml,
@@ -56,7 +109,7 @@ module.exports = function(params) {
       if (module.verbose) {
         console.log('POST ' + url + ' with XML ...');
       }
-      module.client.post(url, args, function(data, response) {
+      module.client.post(url, args, function (data, response) {
 
         if (response.statusCode === 401) {
           reject("401 - API key required");
@@ -103,9 +156,9 @@ module.exports = function(params) {
   };
 
   /*
-  PUT /bookings/{bookingId}
+   PUT /bookings/{bookingId}
    */
-  module.put = function(bid, bxml) {
+  module.put = function (bid, bxml) {
 //    todo:
   };
 
