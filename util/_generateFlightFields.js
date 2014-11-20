6c1f770254ea9496e23a26e44a0f42a67ccc0ec0
Alexey Zhelonkin <Alexey.Zhelonkin@paxport.se>
2014-11-20 13:10:40 +0100

    add random to arrival and departure dates

--- util/_generateFlightFields.js
+++ util/_generateFlightFields.js
@@ -14,14 +14,14 @@ var flightFields = {
 //todo get airports from somewhere
 //todo get service types from somewhere
 exports.get = function(prefix) {
-  var depDate = (new Date.tomorrow()).addMonths(1);
+  var depDate = (new Date.tomorrow()).addMonths(Math.floor(Math.random() * 4) + 1);
   flightFields.prefix = prefix;
   flightFields.flno = flnogen.flnogen(3, prefix);
   flightFields.departureAirport = "ARN";
   flightFields.arrivalAirport = "BKK";
   flightFields.serviceType = "C";
   flightFields.departureDate = depDate.toJSON()
-  flightFields.arrivalDate = depDate.clone().addDays(12).toJSON();
+  flightFields.arrivalDate = depDate.clone().addHours(Math.floor(Math.random() * 6) + 1).toJSON();
 
   return flightFields;
 };
