f6b5dbcd79b5e6a13f3b390ebc9b4f6737b697e7
Alexey Zhelonkin <Alexey.Zhelonkin@paxport.se>
2014-11-20 12:13:23 +0100

    added parameters to the booking.xml
    changed coffee classes

--- scenarios/create-update-flight/scenario.js
+++ scenarios/create-update-flight/scenario.js
@@ -27,12 +27,12 @@ console.log('Wanna create a flight?'.blue);
 var raw = fs.readFileSync(__dirname + '/flight.xml', { encoding: 'UTF8' });
 var prefix = 'TST';
 var flFields = flgen.get(prefix);
-var xml = pd.xmlmin(raw).replace(/\$\{flno\}/g, flFields.flightNumber).replace('${prefix}', prefix).replace('${departureDate}', flFields.departureDate).replace('${arrivalDate}', flFields.arrivalDate)
+var xml = pd.xmlmin(raw).replace(/\$\{flno\}/g, flFields.flno).replace('${prefix}', prefix).replace('${departureDate}', flFields.departureDate).replace('${arrivalDate}', flFields.arrivalDate)
     .replace(/\$\{departureAirport\}/g, flFields.departureAirport).replace('${arrivalAirport}', flFields.arrivalAirport).replace('${serviceType}', flFields.serviceType);
 
 //New depDate should be in 14-15 day from old depDate, but not later then arrivalDate
 var newDepDate = (new Date(flFields.departureDate)).addDays(-5).toJSON();
-var xml_update = pd.xmlmin(raw).replace(/\$\{flno\}/g, flFields.flightNumber).replace('${prefix}', prefix).replace('${departureDate}', newDepDate).replace('${arrivalDate}', flFields.arrivalDate)
+var xml_update = pd.xmlmin(raw).replace(/\$\{flno\}/g, flFields.flno).replace('${prefix}', prefix).replace('${departureDate}', newDepDate).replace('${arrivalDate}', flFields.arrivalDate)
     .replace(/\$\{departureAirport\}/g, flFields.departureAirport).replace('${arrivalAirport}', flFields.arrivalAirport).replace('${serviceType}', flFields.serviceType);
 
 f.post(xml)
