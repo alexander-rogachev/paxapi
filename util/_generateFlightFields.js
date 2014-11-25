cdeede968bb8b28e365cb04964e50b0b64530926
Sergey Lekomtsev <sergey.lekomtsev@fastdev.se>
2014-11-25 12:08:45 +0100

    Changed generate flight util to random values

--- util/_generateFlightFields.js
+++ util/_generateFlightFields.js
@@ -13,14 +13,14 @@ var flightFields = {
 
 //todo get airports from somewhere
 //todo get service types from somewhere
-exports.get = function (prefix) {
-    var depDate = (new Date.tomorrow()).addMonths(Math.floor(Math.random() * 4) + 1);
+exports.get = function (prefix, departureDate) {
+    var depDate = departureDate ? new Date(departureDate) : Date.tomorrow().addMonths(Math.floor(Math.random() * 4) + 1);
     flightFields.prefix = prefix;
     flightFields.flno = flnogen.flnogen(3, prefix);
     flightFields.departureAirport = "ARN";
     flightFields.arrivalAirport = "BKK";
     flightFields.serviceType = "C";
-    flightFields.departureDate = depDate.toJSON();
+    flightFields.departureDate = depDate.toJSON()
     flightFields.arrivalDate = depDate.clone().addHours(Math.floor(Math.random() * 6) + 1).toJSON();
 
     return flightFields;
