1f9b2e919248e10e25923f6428f438e864dce907
Alexander Rogachev <alexander.rogachev@fastdev.se>
2014-12-04 08:47:22 +0100

    add enterprise and paxinput connection strings to properties

--- util/_properties.js
+++ util/_properties.js
@@ -1,4 +1,12 @@
 /**
  * Created by slix on 17.11.2014.
  */
-exports.getBaseUrl = 'http://trigada.paxport.se:8080/openpax2-api/rest';
+//exports.getBaseUrl = 'http://localhost:8080/openpax2-api/rest';
+//exports.getBaseUrl = 'http://trigada.paxport.se:8080/openpax2-api/rest';
+exports.getBaseUrl = 'http://test.paxport.se:8080/openpax2-api/rest';
+//exports.getPaxinputConStr = 'postgres://enterprise:x@localhost:5432/paxinput_local';
+//exports.getPaxinputConStr = 'postgres://enterprise:x@trigada.paxport.se:5432/paxinput_trigada';
+exports.getPaxinputConStr = 'postgres://enterprise:x@test-db.paxport.se:5432/paxinput_test';
+//exports.getEnterpriseConStr = "postgres://enterprise:x@localhost:5432/enterprise_local';
+//exports.getEnterpriseConStr = 'postgres://enterprise:x@trigada.paxport.se:5432/enterprise_trigada';
+exports.getEnterpriseConStr = 'postgres://enterprise:x@test-db.paxport.se:5432/enterprise_test';
